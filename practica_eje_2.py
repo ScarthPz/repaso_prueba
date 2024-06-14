dd44b8a217b7c0628b835c52d47f153d5854fe63
ScarthPz <sca.perezm@duocuc.cl>
2024-06-14 11:05:49 -0400

    ejercicio solo con menú

---
+++ practica_eje_2.py
@@ -0,0 +1,23 @@
+import os, time
+
+while True:
+    print("MENÚ TRABAJADORES")
+    print("-----------------------------------")
+    print("1. Registrar trabajadores")
+    print("2. Listar todos los trabajadores")
+    print("3. Imprimir planilla de sueldos")
+    print("4. Salir del programa")
+    print("-----------------------------------")
+    opc = int(input("Ingrese opción: "))
+    os.system("cls")
+
+    if opc==1:
+        pass
+    elif opc==2:
+        pass
+    elif opc==3:
+        pass
+    else:
+        print("gracias por usar el programa, adios!")
+        break
+    time.sleep(3)
